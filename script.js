b045859eaa062ae915471a8df776bf89ca8324f6
Daniil Shuliyatev <danilashuliyatev@gmail.com>
2024-04-18 16:47:51 +0300

    start

---
+++ script.js
@@ -0,0 +1,37 @@
+"use strict";
+/* let LeftBorderWidth ={
+    width: 5000,
+    length: 10000,
+    height: 2500
+};
+console.log(LeftBorderWidth.height);
+const arr = ["true", "false", "don't find"];
+console.log(arr[2]); */
+/* const result = confirm("what are you doing?");
+/* const a = [1, 2, 3, 4];
+b = a[0];
+console.log(b) 
+console.log(result); */
+/* const result = +prompt('how old are you?', '');
+
+console.log(result); */
+/* let result = [];
+
+result[0] = prompt('how  are you?', '');
+result[1] = prompt('Are you her me?', '');
+result[2] = prompt('why not?', '');
+console.log(result); */
+/* document.write(result); */
+/* const category = 'painter';
+console.log(`https://something.com/${category}/black/0.5`); */
+
+/* let user = 'Daniil';
+
+alert(`Hello, ${user}`) */
+
+let isEnter = true,
+    isExit = true;
+
+/* isEnter;
+isExit; */
+console.log(!isEnter && !isExit)
